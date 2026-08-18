31bd6023dd2baf28b7e1ede37a200981b726b707
sydneybrayanttchabi-lgtm <sydneybrayanttchabi@gmail.com>
2026-08-18 21:51:59 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
- import pandas as pd
- import streamlit.components.v1 as components
+import pandas as pd
+import streamlit.components.v1 as components
 
 # ==========================================
 # 1. CONFIGURATION DE LA PAGE
